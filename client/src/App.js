3a2012576c1855b97e34693984da70ca4ea5abef
Daehyun <dhc4192@gmail.com>
2020-05-15 16:57:55 -0400

    completed shopping list

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import './App.css';
 import { Switch, Route } from "react-router-dom";
-import Layout from './components/shared/Layout';
+import Layout from './Components/shared/Layout';
+import ShoppingList from './Components/AccountMenu/ShoppingList/ShoppingList'
 
 
 function App() {
   return (
     <div className="App">
       <Switch>
-        <Layout></Layout>
+        <Layout>
+        <ShoppingList/>
+        </Layout>
       </Switch>
+      
     </div>
   );
 
--- client/src/Components/AccountMenu/ShoppingList/ShoppingList.jsx
+++ client/src/Components/AccountMenu/ShoppingList/ShoppingList.jsx
@@ -1,49 +1,86 @@
 import React, { useState } from "react";
 import "./ShoppingList.css";
+import AddIcon from "../../../Assets/add-icon.svg";
+import TrashIcon from "../../../Assets/trash-icon.svg";
+import CheckIcon from "../../../Assets/check-icon.svg";
 
-export default function AccountMenu() {
-  const [todos, setTodos] = useState("");
-  const [todoList, setTodoList] = useState([]);
-  const [complete, setComplete] = useState(false);
+export default function ShoppingList(item, index) {
+  const [value, setValue] = useState("");
+  const [items, setItem] = useState([
+    {
+      name: "",
+      completed: true,
+    },
+  ]);
 
-  const handleChange = (e) => {
-    e.preventDefault();
-    setTodos(e.target.value);
+  const addItem = (name) => {
+    const newItems = [...items, { name, completed: false }];
+    setItem(newItems);
+  };
+
+  const completeItem = (index) => {
+    const newItems = [...items];
+    newItems[index].completed = true;
+    setItem(newItems);
+  };
+
+  const removeItem = (index) => {
+    const newItems = [...items];
+    newItems.splice(index, 1);
+    setItem(newItems);
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (todos === "") return;
-    setTodoList([...todoList, { id: Date.now(), text: todos }]);
-    e.target.reset();
-  };
-
-  const removeTodo = (id) => {
-    setTodoList(todoList.filter((todo) => todo.id != id));
-  };
-
-  const completeTodo = () => {
-    setComplete(!complete);
+    if (!value) return;
+    addItem(value);
+    setValue("");
   };
 
   return (
-    <div>
-      <h1>Shopping List</h1>
-      <form onSubmit={handleSubmit}>
-        <input type="text" placeholder="Add Item" onChange={handleChange} />
-        <ul>
-          {todoList.map((todo) => (
-            <li key={todo.id}>
-              {todo.text}
+    <>
+      <h1 className="Shopping-List">Shopping List</h1>
+      <div className="Shopping-List-Form">
+        <div className="Input-Section">
+          <img className="Add-Icon" src={AddIcon} />
 
-              <span onClick={completeTodo} className={complete ? "done" : ""}>
-                Y
-              </span>
-              <span onClick={() => removeTodo(todo.id)}>X</span>
+          <form onSubmit={handleSubmit}>
+            <input
+              className="Shopping-List-Input"
+              type="text"
+              value={value}
+              placeholder="Add Item"
+              onChange={(e) => setValue(e.target.value)}
+            />
+          </form>
+        </div>
+        <div className="List">
+          {items.map((item, index) => (
+            <li
+              className={item.completed ? "Complete" : "Shopping-List-Items"}
+              key={index}
+            >
+              {item.name}
+              <button
+                className={item.completed ? "Deleted" : "Shopping-List-Delete"}
+                onClick={() => removeItem(index)}
+              >
+                <span className="Trash">
+                  <img className="TrashIcon" src={TrashIcon} />
+                </span>
+              </button>
+              <button
+                className={item.completed ? "Checked" : "Shopping-List-Complete"}
+                onClick={() => completeItem(index)}
+              >
+                <span className="Check">
+                  <img className="CheckIcon" src={CheckIcon} />
+                </span>
+              </button>
             </li>
           ))}
-        </ul>
-      </form>
-    </div>
+        </div>
+      </div>
+    </>
   );
 }
