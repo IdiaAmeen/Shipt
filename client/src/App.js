bbfe24124db11ab7778ffdbbb9188b87df982376
Daehyun <dhc4192@gmail.com>
2020-05-15 23:03:19 -0400

    started account menu

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,25 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 import { Switch, Route } from "react-router-dom";
-import Layout from './Components/shared/Layout';
-import ShoppingList from './Components/AccountMenu/ShoppingList/ShoppingList'
-
+import Layout from "./Components/shared/Layout";
+import ShoppingList from "./Components/AccountMenu/ShoppingList/ShoppingList";
+import AccountMenu from "./Components/AccountMenu/AccountMenu";
 
 function App() {
   return (
     <div className="App">
       <Switch>
         <Layout>
-        <ShoppingList/>
+          <Route exact path='/'>
+            <AccountMenu />
+          </Route>
+          <Route path="/ShoppingList">
+            <ShoppingList />
+          </Route>
         </Layout>
       </Switch>
-      
     </div>
   );
-
 }
 
 export default App;
--- client/src/Components/AccountMenu/AccountMenu.jsx
+++ client/src/Components/AccountMenu/AccountMenu.jsx
@@ -1,1 +1,91 @@
+import React, { useState } from "react";
+import ShoppingList from "./ShoppingList/ShoppingList";
+import { Link } from "react-router-dom";
+import "./AccountMenu.css";
+import AccountIcon from "../../Assets/account-icon.svg";
+import AddressIcon from "../../Assets/address-icon.svg";
+import PaymentIcon from "../../Assets/payments-icon.svg";
+import OrderHistoryIcon from "../../Assets/order-history-icon.svg";
+import RewardsIcon from "../../Assets/rewards-icon.svg";
+import ShoppingListIcon from "../../Assets/shopping-list-icon.svg";
+import LogoutIcon from "../../Assets/logout-icon.svg";
 
+export default function AccountMenu() {
+  // const [toggleOn, setToggleOn] = useState(false);
+  // const [style, setStyle] = useState({});
+  // const divStyle1 = {
+  //   display: "block",
+  //   transition: "opacity 2s ",
+  //   opacity: "1",
+  // };
+  // const divStyle2 = {
+  //   transition: "opacity 0.2s ",
+  //   opacity: "0",
+  //   height: "0",
+  // };
+  // const toggleHamburgerMenu = () => {
+  //   toggleOn ? setToggleOn(false) : setToggleOn(true);
+  //   toggleStyle();
+  // };
+  // const toggleStyle = () => {
+  //   if (toggleOn) {
+  //     setStyle(divStyle1);
+  //   } else {
+  //     setStyle(divStyle2);
+  //   }
+  // };
+  return (
+    <div>
+      <nav>
+        {/* <button onClick={toggleHamburgerMenu}>Click Me</button> */}
+        <div className="Account-Menu-Container">
+          <ul className="Account-Menu-UL">
+            <li className="Account-Menu-List">
+              <span className="List-Name">Profile</span>
+              <span className="Menu-Icon-Span">
+                <img className="Menu-Icon" src={AccountIcon} />
+              </span>
+            </li>
+            <li className="Account-Menu-List">
+              <span className="List-Name">Addresses</span>
+              <span className="Menu-Icon-Span">
+                <img className="Menu-Icon" src={AddressIcon} />
+              </span>
+            </li>
+            <li className="Account-Menu-List">
+              <span className="List-Name">Payments</span>
+              <span className="Menu-Icon-Span">
+                <img className="Menu-Icon" src={PaymentIcon} />
+              </span>
+            </li>
+            <li className="Account-Menu-List">
+              <span className="List-Name">Order History</span>
+              <span className="Menu-Icon-Span">
+                <img className="Menu-Icon" src={OrderHistoryIcon} />
+              </span>
+            </li>
+            <li className="Account-Menu-List">
+              <span className="List-Name">Rewards</span>
+              <span className="Menu-Icon-Span">
+                <img className="Menu-Icon" src={RewardsIcon} />
+              </span>
+            </li>
+            <Link className="Shopping-List-Link" to={"/ShoppingList"}>
+              <span className="Shopping-List-Name">Shopping List</span>
+              <span className="Shopping-List-Icon">
+                <img className="Shopping-List-Icon" src={ShoppingListIcon} />
+              </span>
+            </Link>
+            <li className="Account-Menu-List">
+              <span className="List-Name">Log Out</span>
+              <span className="Menu-Icon-Span">
+                <img className="Menu-Icon" src={LogoutIcon} />
+              </span>
+            </li>
+            <div className="Remainder-List"></div>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  );
+}
