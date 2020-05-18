535a499b26ab5543985420145a920745306a4017
Saadat Taaseen <taaseen.homework@gmail.com>
2020-05-18 12:28:30 -0400

    added css to desktop

--- client/src/Components/Credentials/Sign-in.jsx
+++ client/src/Components/Credentials/Sign-in.jsx
@@ -40,46 +40,55 @@ export default function SignIn(props) {
 
     else {
         return (
-            <div>
+            <div >
                 <Layout>
-                    <div style={{ justifyContent: "center", display: "flex" }}>
-                        <div className="sign-in-area">
-                            <h1 className="signUpHeader1"> Welcome Back!</h1>
-                            <p className="signUpHeader2">Your day is about to get a whole lot easier.</p>
-                            <StaticButtons />
-
-                            <p style={{ color: "#838383" }}>_____________________or_____________________</p>
-                            {/* Guard Operator */}
-                            <p style={{ color: "red", fontWeight: "900" }}>
-
-                                {errorMessage && errorMessage}
-                            </p>
-                            <div className="log-in-input">
-                                <p className="inputField">Username:</p>
-                                <input
-                                    type="text"
-                                    className="inputBox" placeholder="Enter your username" onChange={(e) => setUsername(e.target.value)}
-                                ></input>
+                    <div style={{ display: "flex", justifyContent: "center", padding: "50px" }}>
+                        <div className="wrapperSignUp">
+                            <div className="allSignUpHeaders">
+                                <div className="headers">
+                                    <h1 className="signUpHeader1"> Welcome Back!</h1>
+                                    <p className="signUpHeader2">Your day is about to get a whole lot easier.</p>
+                                </div>
+                                <img className="signUpImage" src="Images/SignInimage.png" alt="Sign In Image" width="500px" />
                             </div>
-                            <div className="log-in-input">
-                                <p className="inputField">Password:</p>
-                                <input
-                                    type="password"
-                                    className="inputBox" placeholder="Enter your password" onChange={(e) => setPassword(e.target.value)}
-                                ></input>
-                            </div>
-                            <div style={{ paddingTop: "10px", transform: "translateX(-25%)" }}>
-                                <Link to="./change-password" style={{
-                                    color: "black", textDecoration: "none", fontWeight: "900", fontSize: "10px"
-                                }}>
-                                    Forgot Password?
+                            <div style={{ justifyContent: "center", display: "flex" }}>
+                                <div className="sign-in-area">
+                                    <StaticButtons />
+
+                                    <p style={{ color: "#838383" }}>_____________________or_____________________</p>
+                                    {/* Guard Operator */}
+                                    <p style={{ color: "red", fontWeight: "900" }}>
+
+                                        {errorMessage && errorMessage}
+                                    </p>
+                                    <div className="log-in-input">
+                                        <p className="inputField">Username:</p>
+                                        <input
+                                            type="text"
+                                            className="inputBox" placeholder="Enter your username" onChange={(e) => setUsername(e.target.value)}
+                                        ></input>
+                                    </div>
+                                    <div className="log-in-input">
+                                        <p className="inputField">Password:</p>
+                                        <input
+                                            type="password"
+                                            className="inputBox" placeholder="Enter your password" onChange={(e) => setPassword(e.target.value)}
+                                        ></input>
+                                    </div>
+                                    <div style={{ paddingTop: "10px", transform: "translateX(-25%)" }}>
+                                        <Link to="./change-password" style={{
+                                            color: "black", textDecoration: "none", fontWeight: "900", fontSize: "10px"
+                                        }}>
+                                            Forgot Password?
                             </Link>
-                            </div>
-                            <button className="post-button" onClick={signInAndRedirect}>
-                                Log in
+                                    </div>
+                                    <button className="post-button" onClick={signInAndRedirect}>
+                                        Log in
                         </button>
-                            <div>
-                                Don't have an account?<Link to="/sign-up" style={{ color: "rgb(43,122,95)", fontWeight: "900", textDecoration: "none" }}> SignUp </Link>
+                                    <div style={{ paddingBottom: "20px" }}>
+                                        Don't have an account?<Link to="/sign-up" style={{ color: "rgb(43,122,95)", fontWeight: "900", textDecoration: "none" }}> SignUp </Link>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
--- client/src/Components/Credentials/Sign-up.jsx
+++ client/src/Components/Credentials/Sign-up.jsx
@@ -46,46 +46,55 @@ const SignUp = (props) => {
         return (
             <div>
                 <Layout>
-                    <div style={{ justifyContent: "center", display: "flex" }}>
-                        <div className="sign-up-area">
-                            <h1 className="signUpHeader1">Let's Get Started</h1>
-                            <p className="signUpHeader2">Nice to meet you</p>
-                            <StaticButtons />
-                            <p style={{ color: "#838383" }}>_____________________or_____________________</p>
-                            <div className="sign-up-input">
-                                <h3>Sign up with email</h3>
-                                <p className="inputField">Full Name:</p>
-                                <input
-                                    type="text" placeholder="Enter your full name"
-                                    className="inputBox" onChange={(e) => setUserName(e.target.value)}
-                                ></input>
+                    <div style={{ display: "flex", justifyContent: "center", padding: "50px" }} >
+                        <div className="wrapperSignUp">
+                            <div className="allSignUpHeaders">
+                                <div className="headers">
+                                    <h1 className="signUpHeader1">Let's Get Started</h1>
+                                    <p className="signUpHeader2">Nice to meet you</p>
+                                </div>
+                                <img className="signUpImage" src="Images/SignUpImage.png" alt="Sign In Image" width="400px" />
                             </div>
-                            <div className="sign-up-input">
-                                <p className="inputField">Email:</p>
-                                <input
-                                    type="text" placeholder="Enter your email"
-                                    className="inputBox" onChange={(e) => setEmail(e.target.value)}
-                                ></input>
-                            </div>
-                            <div className="sign-up-input">
-                                <p className="inputField">Password:</p>
-                                <input
-                                    type="password" placeholder="atleast 6 characters"
-                                    className="inputBox" onChange={(e) => setPassword(e.target.value)}
-                                ></input>
-                            </div>
-                            <div className="sign-up-input">
-                                <p className="inputField">Confirm Password:</p>
-                                <input
-                                    type="password" placeholder="Re-enter Password"
-                                    className="inputBox" onChange={(e) => setConfirmPassword(e.target.value)}
-                                ></input>
-                            </div>
-                            <button className="post-button" onClick={callSignUp}>
-                                Sign Up
+                            <div style={{ justifyContent: "center", display: "flex" }}>
+                                <div className="sign-up-area">
+                                    <StaticButtons />
+                                    <p style={{ color: "#838383" }}>_____________________or_____________________</p>
+                                    <div className="sign-up-input">
+                                        <h3>Sign up with email</h3>
+                                        <p className="inputField">Full Name:</p>
+                                        <input
+                                            type="text" placeholder="Enter your full name"
+                                            className="inputBox" onChange={(e) => setUserName(e.target.value)}
+                                        ></input>
+                                    </div>
+                                    <div className="sign-up-input">
+                                        <p className="inputField">Email:</p>
+                                        <input
+                                            type="text" placeholder="Enter your email"
+                                            className="inputBox" onChange={(e) => setEmail(e.target.value)}
+                                        ></input>
+                                    </div>
+                                    <div className="sign-up-input">
+                                        <p className="inputField">Password:</p>
+                                        <input
+                                            type="password" placeholder="atleast 6 characters"
+                                            className="inputBox" onChange={(e) => setPassword(e.target.value)}
+                                        ></input>
+                                    </div>
+                                    <div className="sign-up-input">
+                                        <p className="inputField">Confirm Password:</p>
+                                        <input
+                                            type="password" placeholder="Re-enter Password"
+                                            className="inputBox" onChange={(e) => setConfirmPassword(e.target.value)}
+                                        ></input>
+                                    </div>
+                                    <button className="post-button" onClick={callSignUp}>
+                                        Sign Up
                             </button>
-                            <div>
-                                Already have an account?<Link to="/sign-in" style={{ color: "rgb(43,122,95)", fontWeight: "900", textDecoration: "none" }}> Sign In </Link>
+                                    <div style={{ paddingBottom: "20px" }}>
+                                        Already have an account?<Link to="/sign-in" style={{ color: "rgb(43,122,95)", fontWeight: "900", textDecoration: "none" }}> Sign In </Link>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
