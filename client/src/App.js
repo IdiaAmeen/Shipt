3bf91253c6d0ebc0c49115970d4aefdc7c2d1930
Idiat Ameen <idi@idiats-air.home>
2020-05-18 10:25:22 -0400

    conflicts

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,22 @@
-import React from "react";
-import "./App.css";
+import React from 'react';
+import './App.css';
 import { Switch, Route } from "react-router-dom";
-import Layout from "./components/shared/Layout";
-import Modal from "./components/Modal";
-
+import Layout from './Components/shared/Layout';
+import Home from "./Components/Home"
+import Modal from "./Components/Modal"
+​
+​
+​
 function App() {
   return (
     <div className="App">
-      <Layout>
-        <Switch>
-          <Modal />
-        </Switch>
-      </Layout>
+      <Switch>
+        <Route exact path="/" render={() => <Home />} />
+        <Route exact path="/" render={() => <Modal />} />
+      </Switch>
     </div>
   );
+​
 }
-
+​
 export default App;
