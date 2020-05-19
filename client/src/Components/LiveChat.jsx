69f0d6dadc6f963ce653a41e6cd2f0d59002484d
Idiat Ameen <idi@idiats-air.home>
2020-05-19 00:38:31 -0400

    fixed live chat

--- client/src/Components/LiveChat.jsx
+++ client/src/Components/LiveChat.jsx
@@ -1,33 +1,36 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./LiveChat.css";
+import Nav from "./shared/Nav";
 export default function LiveChat() {
   const [isDisplay, setIsDisplay] = useState(true);
 
   if (isDisplay === false) {
-    return null;
+    return (
+      <>
+        <Nav />
+      </>
+    );
   } else {
     return (
-      <div className="outer-modal1">
-        <div className="inner-modal1">
-          <Link to="/" className="x1">
-            X
-          </Link>
-          <div className="member-shopper1">
-            <div id="member1">Member </div>
-            <div id="shopper1">Shopper </div>
-          </div>
-          <div className="chat-box1">
-            <div className="greeting1">
-              <p>Hi there!</p>
-              <p>How can I help you?</p>
-            </div>
-            <div className="text-box1">
-              <input type="text" />
-            </div>
-          </div>
-          <button className="send"> Send </button>
+      <div className="inner-modal1">
+        <button onClick={() => setIsDisplay(!isDisplay)} className="x1">
+          X
+        </button>
+        <div className="member-shopper1">
+          <button id="member1">Member </button>
+          <button id="shopper1">Shopper </button>
         </div>
+        <div className="chat-box1">
+          <div className="greeting1">
+            <p>Hi there!</p>
+            <p>How can I help you?</p>
+          </div>
+          <div className="text-box1">
+            <input type="text" />
+          </div>
+        </div>
+        <button className="send"> Send </button>
       </div>
     );
   }
--- client/src/components/shared/Nav.js
+++ client/src/components/shared/Nav.js
@@ -5,63 +5,78 @@ import AccountMenu from "../AccountMenu/AccountMenu";
 import HelpIcon from "../../Assets/help-icon.svg";
 import LiveChat from "../LiveChat";
 
-export default function Nav() {
+export default function Nav(props) {
   const [showMenu, setShowMenu] = useState(false);
-  const [isDisplay, setIsDisplay] = useState(false);
-  return (
-    <div>
-      <div className="nav-border">
-        <div className="nav-container">
-          <img
-            className="nav-shipt-logo"
-            src="/Images/brand-elements.png"
-          ></img>
-          <div className="nav-links-container">
-            <Link className="nav-question-mark" to="/">
-              <img src={HelpIcon} className="nav-help-icon" />
-            </Link>
-            <div className="mobile-menu">
-              <button
-                className="nav-account-icon"
-                onClick={() => setShowMenu(!showMenu)}
-              >
-                <img
-                  src="/Images/account-icon@2x.png"
+  let [isDisplay, setIsDisplay] = useState(false);
+  if (isDisplay === true) {
+    return (
+      <div>
+        <LiveChat />
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <div className="nav-border">
+          <div className="nav-container">
+            <img
+              className="nav-shipt-logo"
+              src="/Images/brand-elements.png"
+            ></img>
+            <div className="nav-links-container">
+              <Link className="nav-question-mark" to="/">
+                <img src={HelpIcon} className="nav-help-icon" />
+              </Link>
+              <div className="mobile-menu">
+                <button
                   className="nav-account-icon"
+                  onClick={() => setShowMenu(!showMenu)}
+                >
+                  <img
+                    src="/Images/account-icon@2x.png"
+                    className="nav-account-icon"
+                  />
+                </button>
+
+                <div className="drop-down">{showMenu && <AccountMenu />}</div>
+              </div>
+
+              <Link to="/ShoppingList" className="desktop-nav-container">
+                <img
+                  className="desktop-nav-account-icon"
+                  src="/Images/account-icon@2x.png"
                 />
-              </button>
-
-              <div className="drop-down">{showMenu && <AccountMenu />}</div>
+                <span className="desktop-nav-account-span">Account</span>
+              </Link>
             </div>
-
-            <Link to="/ShoppingList" className="desktop-nav-container">
-              <img
-                className="desktop-nav-account-icon"
-                src="/Images/account-icon@2x.png"
-              />
-              <span className="desktop-nav-account-span">Account</span>
-            </Link>
           </div>
         </div>
-      </div>
-      <div className="nav-services-container">
-        <div className="nav-services-icons">
+        <div className="nav-services-container">
+          <div className="nav-services-icons">
+            <Link to="">
+              <img src="/images/Categories.png"></img>
+            </Link>
+            <Link to="">
+              <img src="/images/SearchIcon.png"></img>
+            </Link>
+          </div>
+
+          {/* <Link to="/livechat">
+          <img src="/images/1381552 2.png" />
+        </Link> */}
+
+          <button
+            className="nav-services-icons"
+            onClick={() => setIsDisplay(!isDisplay)}
+          >
+            <img src="/images/1381552 2.png" className="nav-services-icons" />
+          </button>
+
           <Link to="">
-            <img src="/images/Categories.png"></img>
-          </Link>
-          <Link to="">
-            <img src="/images/SearchIcon.png"></img>
+            <img src="/images/Shopping Cart.png"></img>
           </Link>
         </div>
-
-        <Link to="/livechat">
-          <img src="/images/1381552 2.png" />
-        </Link>
-
-        <Link to="">
-          <img src="/images/Shopping Cart.png"></img>
-        </Link>
       </div>
-    </div>
-  );
+    );
+  }
 }
