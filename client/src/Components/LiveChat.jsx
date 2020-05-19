f6d7ae5e214049d619ecdbc39a383528af84b56c
Idiat Ameen <idi@idiats-air.home>
2020-05-19 10:08:58 -0400

    styling live chat

--- client/src/Components/LiveChat.jsx
+++ client/src/Components/LiveChat.jsx
@@ -13,24 +13,28 @@ export default function LiveChat() {
     );
   } else {
     return (
-      <div className="inner-modal1">
-        <button onClick={() => setIsDisplay(!isDisplay)} className="x1">
-          X
-        </button>
-        <div className="member-shopper1">
-          <button id="member1">Member </button>
-          <button id="shopper1">Shopper </button>
-        </div>
-        <div className="chat-box1">
-          <div className="greeting1">
-            <p>Hi there!</p>
-            <p>How can I help you?</p>
+      <div className="outer-modal">
+        <div className="inner-modal1">
+          <div className="quit">
+            <button onClick={() => setIsDisplay(!isDisplay)} className="x1">
+              X
+            </button>
           </div>
-          <div className="text-box1">
-            <input type="text" />
+          <div className="member-shopper1">
+            <button id="member1">Member </button>
+            <button id="shopper1">Shopper </button>
           </div>
+          <div className="chat-box1">
+            <div className="greeting1">
+              <p>Hi there!</p>
+              <p>How can I help you?</p>
+            </div>
+            <div>
+              <input className="text-box1" type="text" />
+            </div>
+          </div>
+          <button className="send"> Send </button>
         </div>
-        <button className="send"> Send </button>
       </div>
     );
   }
