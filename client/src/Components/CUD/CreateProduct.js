2354bf9b6554ca9dd208345b9355600d77c3364b
Saadat Taaseen <taaseen.homework@gmail.com>
2020-05-19 21:47:55 -0400

    added createdProduct and created css files for it. Header1 needs fontWeight

--- client/src/Components/CUD/CreateProduct.js
+++ client/src/Components/CUD/CreateProduct.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import Layout from "../shared/Layout";
 import { Redirect } from "react-router-dom";
 import { createProduct } from "../../services/product";
+import "./CreateProduct.css"
 
 
 const CreateProduct = (props) => {
@@ -32,26 +33,32 @@ const CreateProduct = (props) => {
         return (
             <div>
                 <Layout>
-                    <div>
-                        <div className='post-area'>
-                            <div className="cudCreateProduct">
-                                <h4> name: </h4>
-                                <input type="text" onChange={(e) => setName(e.target.value)} />
+                    <div id="cudWrapperWrapper">
+                        <div id="cudWrapper">
+                            <h1 >
+                                Add Product Information Here:
+                            </h1>
+
+                            <div className='cudPostArea'>
+                                <div >
+                                    <h4 className="cudInputField"> name: </h4>
+                                    <input className="cudInputBox" type="text" onChange={(e) => setName(e.target.value)} />
+                                </div>
+                                <div >
+                                    <h4 className="cudInputField"> Image Link: </h4>
+                                    <input className="cudInputBox" type="text" onChange={(e) => setImgURL(e.target.value)} />
+                                </div>
+                                <div>
+                                    <h4 className="cudInputField"> Measurement: </h4>
+                                    <input className="cudInputBox" type="text" onChange={(e) => setMeasurement(e.target.value)} />
+                                </div>
+                                <div>
+                                    <h4 className="cudInputField"> Price: </h4>
+                                    <input className="cudInputBox" type="text" onChange={(e) => setPrice(e.target.value)} />
+                                </div>
                             </div>
-                            <div className="cudCreateProduct">
-                                <h4> Image Link: </h4>
-                                <input type="text" onChange={(e) => setImgURL(e.target.value)} />
-                            </div>
-                            <div className="cudCreateProduct">
-                                <h4> Measurement: </h4>
-                                <input type="text" onChange={(e) => setMeasurement(e.target.value)} />
-                            </div>
-                            <div className="cudCreateProduct">
-                                <h4> Price: </h4>
-                                <input type="text" onChange={(e) => setPrice(e.target.value)} />
-                            </div>
+                            <button className="cudPostButton" onClick={callCreateProduct}> Add Product </button>
                         </div>
-                        <button className="cudPostButton" onClick={callCreateProduct}> Submit </button>
                     </div>
                 </Layout>
             </div>
