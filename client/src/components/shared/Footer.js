a384a8c7f846e864c84d23b9583d49869fd737da
Saadat Taaseen <taaseen.homework@gmail.com>
2020-05-20 10:44:55 -0400

    removed links from header in app.js

--- client/src/components/shared/Footer.js
+++ client/src/components/shared/Footer.js
@@ -3,30 +3,31 @@ import "./Footer.css"
 import { Link } from "react-router-dom"
 
 export default function Footer() {
-  return (
-    <div className="footer-container">
-      <img className="footer-logo" src="/Images/Footer logo.png"></img>
-      <div className="sign-links">
-        <Link to="/sign-up"> Sign-up </Link>
-        <Link to="/sign-in"> Sign-in </Link>
-      </div>
-      <div className="credits">
-        <div className="credits-group">
-          <h2>Dev Team</h2>
-          <p>Ashley Madera</p>
-          <p>Daehyun Choi</p>
-          <p>Felipe Lins</p>
-          <p>Idia Ameen</p>
-          <p>Saadat Taaseen</p>
+    return (
+        <div className="footer-container">
+            <img className="footer-logo" src="/Images/Footer logo.png"></img>
+            <div className="sign-links">
+                <Link to="/sign-up"> Sign-up </Link>
+                <Link to="/sign-in"> Sign-in </Link>
+
+            </div>
+            <div className="credits">
+                <div className="credits-group">
+                    <h2>Dev Team</h2>
+                    <p>Ashley Madera</p>
+                    <p>Daehyun Choi</p>
+                    <p>Felipe Lins</p>
+                    <p>Idia Ameen</p>
+                    <p>Saadat Taaseen</p>
+                </div>
+                <div className="credits-group">
+                    <h2>UX Team</h2>
+                    <p>Christine Idrovo</p>
+                    <p>Sam Colonna</p>
+                    <p>Shreya Perti</p>
+                    <p>Yuki Mochizuki</p>
+                </div>
+            </div>
         </div>
-        <div className="credits-group">
-          <h2>UX Team</h2>
-          <p>Christine Idrovo</p>
-          <p>Sam Colonna</p>
-          <p>Shreya Perti</p>
-          <p>Yuki Mochizuki</p>
-        </div>
-      </div>
-    </div>
-  )
+    )
 }
