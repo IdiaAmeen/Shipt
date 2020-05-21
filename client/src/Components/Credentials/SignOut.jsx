c8d60bc565d141056f6eafff4eff234437f608b6
Daehyun <dhc4192@gmail.com>
2020-05-21 16:24:16 -0400

    finished up css

--- client/src/Components/Credentials/SignOut.jsx
+++ client/src/Components/Credentials/SignOut.jsx
@@ -1,17 +1,17 @@
-import React, { useEffect } from 'react'
-import { signOut } from '../../services/user'
-import { useHistory } from 'react-router-dom'
+import React, { useEffect } from "react";
+import { signOut } from "../../services/user";
+import { useHistory } from "react-router-dom";
 
 export default function SignOut(props) {
-    const { clearUser, user } = props
-    const history = useHistory()
+  const { clearUser, user } = props;
+  const history = useHistory();
 
-    useEffect(() => {
-        signOut(user)
-            .then(() => localStorage.setItem("token", ""))
-            .then(() => props.setCurrentUser(null))
-            .finally(() => history.push("/"))
-    })
+  useEffect(() => {
+    signOut(user)
+      .then(() => localStorage.setItem("token", ""))
+      .then(() => props.setCurrentUser(null))
+      .finally(() => history.push("/"));
+  });
 
-    return ""
+  return "";
 }
